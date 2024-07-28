773ab2650917f7f61c213edb6ba96a15d212a5db
Pretisha Sahoo <pretishasahoo@gmail.com>
2024-07-28 09:38:52 +0530

    userschema changed a bit

--- Models/User.js
+++ Models/User.js
@@ -5,7 +5,6 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     uid: { type: String, required: true },
-    isAdmin:{type:Boolean , default:false},
     isDoctor:{type:Boolean , default:false},
     notification:{type:Array , default:[]},
     seennotification:{type:Array , default:[]}
