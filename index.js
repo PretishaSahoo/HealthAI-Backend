6711428c8eb88f777efeeeda78c0ccbaef968e22
Pretisha Sahoo <pretishasahoo@gmail.com>
2024-08-04 15:44:22 +0530

    undo

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const http = require("http");
-const { Server } = require("socket.io");
 const connectToMongo = require("./db");
-
-const isLocal = process.env.MODE === 'development';
+const checkAndSendReminders = require('./NotificationManagement/Notifications'); 
+const cron = require('node-cron');
 
 dotenv.config();
 
@@ -16,7 +14,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.options("*", cors({
+app.options("", cors({
     origin: ["http://localhost:3000", "https://health-ai-teal.vercel.app"],
     methods: ['POST', 'DELETE', 'GET', 'PUT', 'PATCH'],
     credentials: true
@@ -25,19 +23,10 @@ app.options("*", cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:3000", "https://health-ai-teal.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-        credentials: true
-    }
-});
-
-io.on("connection", (socket) => {
-    console.log(`Socket Connected: ${socket.id}`);
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
@@ -52,15 +41,7 @@ const DoctorRouter = require("./Routes/Doctor");
 app.use("/api/user", UserRouter.router);
 app.use("/api/doctor", DoctorRouter.router);
 
-if (isLocal) {
-    const cron = require('node-cron');
-    const checkAndSendReminders = require('./NotificationManagement/Notifications');
-
-    cron.schedule('* * * * *', () => {
-        checkAndSendReminders();
-    });
-}
-
-server.listen(PORT, () => {
-    console.log(`Server running on Port ${PORT}`);
+// Scheduled the checkAndSendReminders function to run every minute
+cron.schedule('* * * * *', () => {
+    checkAndSendReminders();
 });
